5836a95b45d2d5beccf22beb991a90a1b2a3ec2e
Kyle Ezekiel Moreno <151549802+kinatulinan@users.noreply.github.com>
2025-04-09 09:45:17 +0800

    Update ArchiquestApplication.java

--- Archiquest Backend/src/main/java/com/ArchiquestBackend/Archiquest/ArchiquestApplication.java
+++ Archiquest Backend/src/main/java/com/ArchiquestBackend/Archiquest/ArchiquestApplication.java
@@ -3,6 +3,7 @@ package com.ArchiquestBackend.Archiquest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 @SpringBootApplication(scanBasePackages = "com.ArchiquestBackend.Archiquest")
 public class ArchiquestApplication {
 
