1da9608cce0800c4e3cf2e9f48bdfcee616fa440
kinatulinan <151549802+kinatulinan@users.noreply.github.com>
2025-04-09 09:33:34 +0800

    Test

--- Archiquest Backend/src/main/java/com/ArchiquestBackend/Archiquest/ArchiquestApplication.java
+++ Archiquest Backend/src/main/java/com/ArchiquestBackend/Archiquest/ArchiquestApplication.java
@@ -3,7 +3,7 @@ package com.ArchiquestBackend.Archiquest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.ArchiquestBackend.Archiquest")
 public class ArchiquestApplication {
 
 	public static void main(String[] args) {
